5641badc88f2981c6e3df41d8b965143af6cd414
dhanalaxmi <dhanalaxmidupakuntla@gmail.com>
2026-02-25 12:51:03 +0530

    Initial banckend Project

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,6 @@ import { registerUser, loginUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post(
   "/register",
