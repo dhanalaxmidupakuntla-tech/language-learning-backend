6f9f11e17a5782d92287cebaa46b4f7174fc9343
dhanalaxmi <dhanalaxmidupakuntla@gmail.com>
2026-02-26 21:27:59 +0530

    backend initialtion

--- routes/lessonRoutes.js
+++ routes/lessonRoutes.js
@@ -1,10 +1,9 @@
 import express from "express";
-import authMiddleware from "../middleware/authMiddleware.js";
 import { fetchLessons, fetchLessonById } from "../controllers/lessonController.js";
 
 const router = express.Router();
 
-router.get("/", authMiddleware, fetchLessons);
-router.get("/:id", authMiddleware, fetchLessonById);
+router.get("/", fetchLessons);
+router.get("/:id", fetchLessonById);
 
 export default router;
